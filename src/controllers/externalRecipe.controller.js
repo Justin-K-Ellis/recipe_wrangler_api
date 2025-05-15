4f9aee09b658a9ef8a1fac382de2093aa04deff7
Justin-K-Ellis <jlke45@gmail.com>
2025-05-15 22:13:58 +0900

    feat: create model & controller for fetching recipe details

--- src/controllers/externalRecipe.controller.js
+++ src/controllers/externalRecipe.controller.js
@@ -1,18 +1,30 @@
 import express from "express";
-import { getRecipeBySearch } from "../models/externalRecipe.model.js";
+import externalRecipeModel from "../models/externalRecipe.model.js";
 
 const externalRecipeController = express.Router();
 
 externalRecipeController.get("/:searchTerm", async (req, res) => {
   const { searchTerm } = req.params;
   try {
-    const result = await getRecipeBySearch(searchTerm);
+    const result = await externalRecipeModel.getRecipeBySearch(searchTerm);
     console.log(result);
     res.json(result);
   } catch (error) {
     console.error(error);
     res.status(500);
-    res.json(":(");
+    res.json("Something went wrong when searching for food.");
+  }
+});
+
+externalRecipeController.get("/id/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await externalRecipeModel.getRecipeById(id);
+  res.json(result);
+  try {
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.json({ message: "Something went wrong when fetching food by id." });
   }
 });
 
--- src/models/externalRecipe.model.js
+++ src/models/externalRecipe.model.js
@@ -1,13 +1,15 @@
 import dotenv from "dotenv";
+
 dotenv.config();
+const apiKey = process.env.SPOONTACULAR_API;
 
 async function getRecipeBySearch(searchTerm) {
-  const searchString = `https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&instructionsRequired=true`;
-  const response = await fetch(searchString, {
+  const query = `https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&instructionsRequired=true`;
+  const response = await fetch(query, {
     method: "GET",
     headers: {
       "Content-type": "application/json",
-      "x-api-key": process.env.SPOONTACULAR_API,
+      "x-api-key": apiKey,
     },
   });
   const data = await response.json();
@@ -17,4 +19,35 @@ async function getRecipeBySearch(searchTerm) {
   return results;
 }
 
-export { getRecipeBySearch };
+async function getRecipeById(id) {
+  const query = `https://api.spoonacular.com//recipes/${id}/information?includeNutrition=false`;
+  const response = await fetch(query, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      "x-api-key": apiKey,
+    },
+  });
+  const data = await response.json();
+
+  const ingredients = data.extendedIngredients.map(
+    (ing) =>
+      `${ing.name} (${ing.measures.metric.amount} ${ing.measures.metric.unitLong})`
+  );
+  const steps = data.analyzedInstructions[0].steps.map((stepObj) => {
+    return stepObj.step;
+  });
+
+  const result = {
+    id: data.id,
+    name: data.title,
+    ingredients: ingredients || [],
+    steps: steps || [],
+    readyInMinutes: data.readyInMinutes || 0,
+  };
+  return result;
+}
+
+const externalRecipeModel = { getRecipeBySearch, getRecipeById };
+
+export default externalRecipeModel;
