9a670ef843e6053640bb770e21e1079bb0e97c01
Justin-K-Ellis <jlke45@gmail.com>
2025-06-27 14:52:43 +0900

    refactor: explicitly set CORS origin for client URL & localhost

--- src/app.js
+++ src/app.js
@@ -10,7 +10,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, "http://localhost:3000"],
+  })
+);
 
 app.use("/api", authenticateToken, router);
 
